7c081a89fa79b23f8af486aa18316b313523d8fe
Jeff Arends <jeffrey.arends@mso.umt.edu>
2023-06-12 11:59:11 -0600

    Getting rid of error messages on swipe

--- rfid/readrfid.py
+++ rfid/readrfid.py
@@ -3,13 +3,15 @@ import gpiod
 
 reader = SimpleMFRC522()
 try:
+    reader.__init__()
     print("Tap the a card to the reader to read it (or press CTRL+C to quit)")
     id, text = reader.read()
-    print("Card id: " + id)
+    print("Card id: " + str(id))
     print("Text: " + text)
 except KeyboardInterrupt:
     print("\nExiting due to keyboard interrupt")
 finally:
-    chip = gpiod.chip(1)
-    chip.reset()
+    pass
+#    chip = gpiod.Chip("1")
+#    chip.reset()
 
