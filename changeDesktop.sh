777e9bc96fdf03ec54abcad5db17b1994482551f
Jeff Arends <jeffrey.arends@mso.umt.edu>
2023-06-26 16:58:14 -0600

    added ability to change to xfce desktop

---
+++ changeDesktop.sh
@@ -0,0 +1,9 @@
+#sudo sed -i 's/noquiet splash"/noquiet splash 3"/g' /etc/default/grub
+#sudo update-grub
+#sudo apt install -y xfce4-session xfce4-goodies x11-apps xfce4-panel xinit
+#sudo apt install qterminal firefox
+
+echo 'export PYTHONPATH="${PYTHONPATH}:~/STEMCamp-LePotato"
+if [[ "$(tty)" = "/dev/tty1" ]]; then
+	pgrep xfce || startxfce4
+fi' >> ~/.bashrc
