ae282b45e6fb6bf5ce8c00bdac5c8064fec80221
Jeff Arends <jeffrey.arends@mso.umt.edu>
2023-06-26 17:08:03 -0600

    fixing the pythonpath

--- changeDesktop.sh
+++ changeDesktop.sh
@@ -3,7 +3,7 @@ sudo update-grub
 sudo apt install -y xfce4-session xfce4-goodies x11-apps xfce4-panel xinit
 sudo apt install qterminal firefox
 
-echo 'export PYTHONPATH="${PYTHONPATH}:~/STEMCamp-LePotato"
+echo 'export PYTHONPATH="${PYTHONPATH}:'$(pwd)'"
 if [[ "$(tty)" = "/dev/tty1" ]]; then
 	pgrep xfce || startxfce4
 fi' >> ~/.bashrc
