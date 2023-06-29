e7ccc533aa11a8371463c385f56b3a186677b62e
lothar243 <jeffarends@gmail.com>
2023-06-29 08:06:49 -0600

    int wasn't iterable

--- neopixelDemo.py
+++ neopixelDemo.py
@@ -20,7 +20,7 @@ colorArray = [(200,0,130), (255,0,0), (255,127,0), (255,255,0), (127,255,0), (0,
 count = 0
 numPixels = 10
 while True:
-  for i in numPixels:
+  for i in range(numPixels):
     pixels[i] = colorArray[(i + count) % numPixels]
   sleep(.2)
   count += 1
