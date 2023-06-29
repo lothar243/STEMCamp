16be7b7ac80b08ca4a464d70bee0fd04158c7c9c
lothar243 <jeffarends@gmail.com>
2023-06-29 08:04:11 -0600

    Update neopixelDemo.py

--- neopixelDemo.py
+++ neopixelDemo.py
@@ -1,5 +1,7 @@
 import board
 import neopixel
+from time import sleep
+
 pixels = neopixel.NeoPixel(board.D18, 30)
 
 pixels.fill((100,0,100))
@@ -14,3 +16,11 @@ pixels[7] = (0,0,255)
 pixels[8] = (75,0,130)
 pixels[9] = (143,0,255)
 
+colorArray = [(200,0,130), (255,0,0), (255,127,0), (255,255,0), (127,255,0), (0,255,0), (0,127,255), (0,0,255), (75,0,130), (143,0,255)]
+count = 0
+numPixels = 10
+while True:
+  for i in numPixels:
+    pixels[i] = colorArray[(i + count) % numPixels]
+  sleep(.2)
+  count += 1
